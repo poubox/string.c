c74e4ca5fa53f3463bda2b8310f572815155779c
poubox <61234611+poubox@users.noreply.github.com>
2021-01-02 15:36:12 +0900

    Update 소문자->대문자.c

--- 소문자->대문자.c
+++ 소문자->대문자.c
@@ -10,3 +10,20 @@ int main()
 		putchar(ch);
 	}
 }
+
+// ctype.h 함수를 이용한 변환
+#include <stdio.h>
+#include <ctype.h>
+// 알파벳 소문자 -> 대문자
+int main()
+{
+	char ch;
+
+	while ((ch = getchar()) != '\n') {
+		if (islower(ch))
+			ch = toupper(ch);
+		else if (isupper(ch))
+			ch = tolower(ch);
+		putchar(ch);
+	}
+}
